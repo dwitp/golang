cb9edcd986e1a68ad89a8fc25e21965a18a3f6ae
Dwi Prabowo <dwi.prabowo@tokopedia.com>
2017-07-28 11:17:33 +0700

    run tool

--- src/run/app.go
+++ src/run/app.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-    "github.com/dwitp/golang/src/u"
+    "fmt"
+    "os"
+    "os/exec"
+    "log"
+    "path"
 )
 
 func main() {
-    u.D("test run...")
+    dirPath, err := os.Getwd()
+    if err != nil {
+        log.Fatal(err)
+    }
+    key := path.Base(dirPath)
+
+    cmd := exec.Command("go", "install")
+    err = cmd.Start()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+
+    if key == "run" {
+        log.Fatal("self install - exiting ...")
+    }
+    out, err := exec.Command(key).Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("%s\n", out)
 }
